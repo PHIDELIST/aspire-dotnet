b4fac0a1bd910f23d3727e2f78552c948216e243
PHIDELIST <phidelisoluoch@gmail.com>
2024-05-08 17:25:22 +0300

    Added react aplication as part of aspire orchest

--- aspire-dotnet.AppHost/Program.cs
+++ aspire-dotnet.AppHost/Program.cs
@@ -6,4 +6,10 @@ builder.AddProject<Projects.aspire_dotnet_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(apiService);
 
+builder.AddNpmApp("react", "../aspire-react")
+    .WithEnvironment("BROWSER", "none") // Disable opening browser on npm start
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
+
 builder.Build().Run();
