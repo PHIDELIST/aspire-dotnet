d1a747fc637d0784049a6cd5bc220778f2bed5e2
PHIDELIST <phidelisoluoch@gmail.com>
2024-05-08 18:45:06 +0300

    integrated Aspire AWS

--- aspire-dotnet.AppHost/Program.cs
+++ aspire-dotnet.AppHost/Program.cs
@@ -1,13 +1,24 @@
+using Amazon;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var apiService = builder.AddProject<Projects.aspire_dotnet_ApiService>("apiservice");
-
+var awsConfig = builder.AddAWSSDKConfig()
+                        .WithProfile("dev")
+                        .WithRegion(RegionEndpoint.USWest2);
+var awsResources = builder.AddAWSCloudFormationTemplate("AspireSampleDevResources", "aws-resources.template")
+                          .WithParameter("DefaultVisibilityTimeout", "30")
+                          .WithReference(awsConfig);
 
 builder.AddNpmApp("react", "../aspire-react")
     .WithReference(apiService)
+    .WithReference(awsResources)
+    .WithReference(awsConfig)
     .WithEnvironment("BROWSER", "none") 
     .WithHttpEndpoint(env: "PORT")
     .WithExternalHttpEndpoints()
     .PublishAsDockerFile();
+
+
 
 builder.Build().Run();
