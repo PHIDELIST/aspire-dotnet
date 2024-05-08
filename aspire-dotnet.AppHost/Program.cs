c2da4ee102b6aaad69dd184f29ee5c1c2254e9eb
PHIDELIST <phidelisoluoch@gmail.com>
2024-05-08 18:07:51 +0300

    retire blazer

--- aspire-dotnet.AppHost/Program.cs
+++ aspire-dotnet.AppHost/Program.cs
@@ -2,12 +2,10 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var apiService = builder.AddProject<Projects.aspire_dotnet_ApiService>("apiservice");
 
-builder.AddProject<Projects.aspire_dotnet_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithReference(apiService);
 
 builder.AddNpmApp("react", "../aspire-react")
-    .WithEnvironment("BROWSER", "none") // Disable opening browser on npm start
+    .WithReference(apiService)
+    .WithEnvironment("BROWSER", "none") 
     .WithHttpEndpoint(env: "PORT")
     .WithExternalHttpEndpoints()
     .PublishAsDockerFile();
