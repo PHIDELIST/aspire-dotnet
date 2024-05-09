f41a1c028fc500dcc12b5c73f64c60ff0f84f920
PHIDELIST <phidelisoluoch@gmail.com>
2024-05-09 17:43:48 +0300

    aws resources

--- aspire-dotnet.AppHost/Program.cs
+++ aspire-dotnet.AppHost/Program.cs
@@ -8,19 +8,17 @@ var awsConfig = builder.AddAWSSDKConfig()
 
 var ddbLocal = builder.AddAWSDynamoDBLocal("ddblocal"); 
 
-var apiService = builder.AddProject<Projects.aspire_dotnet_ApiService>("apiservice")
-                        .WithReference(ddbLocal)
-                        .WithReference(awsConfig);
-
-
 var awsResources = builder.AddAWSCloudFormationTemplate("AspireSampleDevResources", "aws-resources.template")
                           .WithParameter("DefaultVisibilityTimeout", "30")
                           .WithReference(awsConfig);
 
+var apiService = builder.AddProject<Projects.aspire_dotnet_ApiService>("apiservice")
+                        .WithReference(ddbLocal)
+                        .WithReference(awsConfig)
+                        .WithReference(awsResources);
+
 builder.AddNpmApp("react", "../aspire-react")
     .WithReference(apiService)
-    .WithReference(awsResources)
-    .WithReference(awsConfig)
     .WithEnvironment("BROWSER", "none") 
     .WithHttpEndpoint(env: "PORT")
     .WithExternalHttpEndpoints()
